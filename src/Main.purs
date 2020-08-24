3affca31f61744b1a67b843182c378cbd3cbca2f
afcondon <andrew.condon@gmail.com>
2020-08-24 14:12:37 +0200

    successfully pulls keys from map

--- src/Main.purs
+++ src/Main.purs
@@ -6,27 +6,27 @@ import Prelude
 import Data.Argonaut.Core (Json, fromArray, fromObject, fromString, jsonEmptyArray, jsonSingletonObject) as A
 import Data.Argonaut.Decode (decodeJson)
 import Data.Argonaut.Parser (jsonParser)
-import Data.Either (Either)
-import Data.Map (Map, fromFoldable) as M
+import Data.Array as Array
+import Data.Either (Either(..))
+import Data.Map (Map, fromFoldable, keys, lookup) as M
 import Data.Tuple (Tuple(..))
 import Debug.Trace (traceM)
 import Effect (Effect)
 
 foreign import graphObject :: A.Json
 foreign import graphTuples :: A.Json
-foreign import graphArray :: A.Json
--- foreign import jsonString :: String
--- foreign import someObjectFn :: A.Json
 
 
 
--- data Package = { path :: String, depends :: Array String }
+
+type Package = { path :: String, depends :: Array String }
 
 main :: Effect Unit
 main = do
   let
-    a = (decodeJson graphTuples) :: Either String (M.Map String A.Json)
-    -- b = (decodeJson =<< jsonParser jsonString) :: Either String (M.Map String (Array String))
+    a = (decodeJson graphTuples) :: Either String (M.Map String Package)
+    b = case a of
+      (Right m) -> Array.fromFoldable $ M.keys m
+      (Left err) -> ["didn't work"]
   traceM a
-  -- traceM b
-  -- traceM $ a == b
+  traceM b

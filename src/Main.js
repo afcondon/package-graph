bce9a5ae511f629904774c7eff410b42209940d5
afcondon <andrew.condon@gmail.com>
2020-08-24 12:59:30 +0200

    checkpoint commit before trying to build up
    …from caseJsonObject etc directly

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,46 @@
 // Main.js
 var obj = {
-  "Data.BooleanAlgebra": ["a", "b"],
-  "Data.Ring": ["c", "d"]
-};
+  'Data.BooleanAlgebra': ['a', 'b'],
+  'Data.Ring': ['c', 'd']
+}
 
-exports.tuples = Object.entries(obj);
+var graph = {
+  'Data.BooleanAlgebra': {
+    path: '.spago/prelude/v4.1.1/src/Data/BooleanAlgebra.purs',
+    depends: [
+      'Data.HeytingAlgebra',
+      'Data.Symbol',
+      'Data.Unit',
+      'Data.Show',
+      'Record.Unsafe',
+      'Type.Data.RowList',
+      'Type.Data.Row'
+    ]
+  },
+  'Data.Ring': {
+    path: '.spago/prelude/v4.1.1/src/Data/Ring.purs',
+    depends: [
+      'Data.Semiring',
+      'Data.Symbol',
+      'Data.Unit',
+      'Data.Show',
+      'Record.Unsafe',
+      'Type.Data.RowList',
+      'Type.Data.Row'
+    ]
+  }
+}
 
-exports.jsonString = JSON.stringify(exports.tuples);
+exports.someObject = {
+  people: [{ name: 'john' }, { name: 'jane' }],
+  common_interests: []
+}
+
+var toplevel = Object.entries(obj)
+var inner = toplevel.forEach(element => {
+  Object.entries(element) // should get us path / depends
+})
+
+exports.tuples = Object.entries(obj)
+
+exports.jsonString = JSON.stringify(exports.tuples)
--- src/Main.purs
+++ src/Main.purs
@@ -3,22 +3,33 @@ module Main where
 
 import Prelude
 
-import Data.Argonaut.Core (Json)
+import Data.Argonaut.Core (Json, fromArray, fromObject, fromString, jsonEmptyArray, jsonSingletonObject) as A
 import Data.Argonaut.Decode (decodeJson)
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either)
-import Data.Map (Map)
+import Data.Map (Map, fromFoldable) as M
+import Data.Tuple (Tuple(..))
 import Debug.Trace (traceM)
 import Effect (Effect)
 
-foreign import tuples :: Json
+foreign import tuples :: A.Json
 foreign import jsonString :: String
+foreign import someObjectFn :: A.Json
+
+
+someObject = A.fromObject (M.fromFoldable [
+                Tuple "people" (A.fromArray [
+                  A.jsonSingletonObject "name" (A.fromString "john"),
+                  A.jsonSingletonObject "name" (A.fromString "jane")
+                ]),
+                Tuple "common_interests" A.jsonEmptyArray
+              ])
 
 main :: Effect Unit
 main = do
   let
-    a = (decodeJson tuples) :: Either String (Map String (Array String))
-    b = (decodeJson =<< jsonParser jsonString) :: Either String (Map String (Array String))
+    a = (decodeJson tuples) :: Either String (M.Map String (Array String))
+    b = (decodeJson =<< jsonParser jsonString) :: Either String (M.Map String (Array String))
   traceM a
   traceM b
   traceM $ a == b
